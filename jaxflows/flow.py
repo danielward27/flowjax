bd4875eb75a91913cf22d36ce4f00ddbdf039894
danielward27 <danielward27@outlook.com>
2022-02-28 18:19:36 +0000

    Update examples

--- jaxflows/flow.py
+++ jaxflows/flow.py
@@ -42,16 +42,25 @@ class Flow(eqx.Module):
             self.base_log_prob = lambda x: norm.logpdf(x).sum(axis=1)
             self.base_sample = lambda key, n: random.normal(key, (n, target_dim))
 
-    def log_prob(self, x: jnp.array, condition):
+    def log_prob(self, x: jnp.array, condition=None):
         "Evaluate the log probability of the target distribution."
+        if condition is None:
+            condition = jnp.zeros((x.shape[0], 0))  # Empty placeholder
+        return self._log_prob(x, condition)
+
+    @eqx.filter_jit
+    def _log_prob(self, x: jnp.array, condition: jnp.array):
         z, log_abs_det = jax.vmap(self.bijection.transform_and_log_abs_det_jacobian)(
             x, condition
         )
         p_z = self.base_log_prob(z)
         return p_z + log_abs_det
 
-    def sample(self, key: random.PRNGKey, n: int, condition):
+    def sample(self, key: random.PRNGKey, n: int, condition=None):
         "Sample from the target distribution."
+        if condition is None:
+            condition = jnp.zeros((n, 0))  # Empty placeholder
+
         z = self.base_sample(key, n)
         x = jax.vmap(self.bijection.inverse)(z, condition)
         return x
