f8306e969f4d8e4e23204c93ebf4836a01ca0411
danielward27 <danielward27@outlook.com>
2022-03-14 15:55:23 +0000

    update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="flowjax",
-    version="0.1.10",
+    version="0.2.0",
     url="https://github.com/danielward27/flowjax.git",
     license="MIT",
     author="Daniel Ward",
