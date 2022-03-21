cf6b1d41a9ffbee0d4b76e65e487c6663967440d
danielward27 <danielward27@outlook.com>
2022-03-21 11:32:57 +0000

    Update block shapes

--- flowjax/bijections/bnaf.py
+++ flowjax/bijections/bnaf.py
@@ -1,48 +1,87 @@
-from typing import Tuple
 import equinox as eqx
 import jax
 import jax.numpy as jnp
 from flowjax.bijections.abc import Bijection
+from jax import random
+from math import prod
+from jax.nn.initializers import glorot_uniform
 
-# Some adapted from https://github.com/pyro-ppl/numpyro/blob/master/numpyro/nn/block_neural_arn.py
 
-
-class BlockMaskedDense(eqx.Module, Bijection):
+class BlockAutoregressiveLinear(eqx.Module, Bijection):
     num_blocks: int
     block_shape: tuple
-    block_diag_mask: jnp.ndarray
+    block_diag_mask: jnp.ndarray  # TODO Do I need to buffer these?
     block_tril_mask: jnp.ndarray
+    W: jnp.ndarray
+    bias: jnp.ndarray
 
-    def __init__(self, num_blocks: int, block_shape: tuple):
+    def __init__(
+        self,
+        key: random.PRNGKey,
+        num_blocks: int,
+        block_shape: tuple,
+        init=glorot_uniform(),
+    ):
         self.block_shape = block_shape
         self.num_blocks = num_blocks
 
-        # TODO need to get input and output dimensions right.
-        self.block_diag_mask = jax.scipy.linalg.block_diag(
-            *[jnp.ones(block_shape, int) for _ in range(num_blocks)]
-        )
-        self.block_tril_mask = tril_block_mask(num_blocks, block_shape)
+        block_shapes = [
+            (block_shape[0], 1),
+            *[block_shape for _ in range(num_blocks - 2)],
+            (1, block_shape[1]),
+        ]
 
-    def transform():
-        pass
+        self.block_diag_mask = jax.scipy.linalg.block_diag(
+            *[jnp.ones(s, int) for s in block_shapes]
+        )
+        self.block_tril_mask = block_tril_mask(block_shapes)
+        w_key, bias_key = random.split(key)
+
+        w_shape = [prod(a) for a in zip(*block_shapes)]
+        self.W = init(w_key, w_shape)*(self.block_tril_mask + self.block_diag_mask)
+        self.bias = init(bias_key, (w_shape[1],))
+
+    def transform(self, x):
+        w = jnp.softplus(self.W) * self.block_diag_mask + self.W * self.block_tril_mask
+        return w @ x + self.bias
 
     def transform_and_log_abs_det_jacobian(self, x, *args, condition=...):
-        return super().transform_and_log_abs_det_jacobian(x, *args, condition=condition)
+        w = jnp.exp(self.W) * self.block_diag_mask + self.W * self.block_tril_mask
+        y = w @ x + self.bias
+
+        log_det = self.W[self.block_diag_mask.astype(bool)]
+
+
+        log_det = 0
+        for i in self.num_blocks:
+            log_det_i = 
+            log_det.append(log_det_i)
+
+        dense_log_det = self.W * self.block_diag_mask
+        jax.nn.logsumexp()
+
+        # Is numpyro missing an exp? logmatmul exp and already reparamed using exp?
+
+        log_abs_det = 1  # TODO calculate log_abs_det here
+        return y, log_abs_det
+
+    # TODO Maybe try bnaf log_abs_det and compare to true using jacobian function
 
     def inverse():
         NotImplementedError("Inverse is not implemented for this bijection.")
 
 
-def tril_block_mask(num_blocks: int, block_shape: tuple):
+def block_tril_mask(block_shapes: list):
     "Lower triangular block mask, excluding diagonal blocks."
     arrays = []
-    for i in range(num_blocks):
+    for i, shape1 in enumerate(block_shapes):
         row = []
-        for j in range(num_blocks):
-            if i > j:
-                row.append(jnp.ones(block_shape))
+        for j, shape2 in enumerate(block_shapes):  # n_rows from
+            if j < i:
+                row.append(jnp.ones((shape1[0], shape2[1])))
             else:
-                row.append(jnp.zeros(block_shape))
+                row.append(jnp.zeros((shape1[0], shape2[1])))
         arrays.append(row)
     return jnp.block(arrays)
+
 
--- tests/test_bijections/test_bnaf.py
+++ tests/test_bijections/test_bnaf.py
@@ -1,16 +1,35 @@
-from flowjax.bijections.bnaf import BlockMaskedDense, tril_block_mask
+from flowjax.bijections.bnaf import BlockAutoregressiveLinear, block_tril_mask
 import jax.numpy as jnp
 import pytest
 
 
+def test_block_tril_mask():
+    block_shapes = [(2, 1), (2, 2), (1, 2)]
+    expected = jnp.array(
+        [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0],
+        ]
+    )
+
+    result = block_tril_mask(block_shapes)
+    assert jnp.all(expected == result)
+
+
 def test_BlockMaskedDense():
-    # Test masks formed correctly
-    layer = BlockMaskedDense(3, (1, 2))
-    expected = jnp.array([[1, 1, 0, 0, 0, 0], [0, 0, 1, 1, 0, 0], [0, 0, 0, 0, 1, 1]])
+    # Test diag mask formed correctly
+    layer = BlockAutoregressiveLinear(3, (2, 2))
+    expected = jnp.array(
+        [
+            [1, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0],
+            [0, 1, 1, 0, 0],
+            [0, 0, 0, 1, 1],
+        ]
+    )
     assert jnp.all(layer.block_diag_mask == expected)
 
-
-def test_tril_block_mask():
-    expected = jnp.array([[0, 0, 0, 0, 0, 0], [1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 0, 0]])
-    result = tril_block_mask(3, (1, 2))
-    assert jnp.all(expected == result)
