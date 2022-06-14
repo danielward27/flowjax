Fix broadcasting bug

flowjax/flows.py
@@ -1,5 +1,6 @@
 from typing import Callable, Optional
 import jax.numpy as jnp
+from numpy import broadcast_shapes
 from flowjax.bijections.abc import Bijection
 from jax.scipy.stats import norm
 from jax import random
@@ -55,13 +56,21 @@ class Flow(eqx.Module):
         x = jnp.atleast_2d(x)
 
         if condition is not None:
-            x, condition = jnp.broadcast_arrays(x, condition)
+            x, condition = self._broadcast(x, condition)
 
         z, log_abs_det = jax.vmap(self.bijection.transform_and_log_abs_det_jacobian)(
             x, condition
         )
         p_z = self.base_log_prob(z)
         return p_z + log_abs_det
+
+    @staticmethod
+    def _broadcast(x: jnp.ndarray, condition: jnp.ndarray):
+        "Broadcast arrays, excluding last axis."
+        s = broadcast_shapes(x.shape[:-1], condition.shape[:-1])
+        x = jnp.broadcast_to(x, s + (x.shape[-1],))
+        condition = jnp.broadcast_to(condition, s + (condition.shape[-1],))
+        return x, condition
 
     @eqx.filter_jit
     def sample(

tests/test_flows.py
@@ -34,20 +34,48 @@ def test_Flow():
     lp1, lp2 = [flow.log_prob(x).item() for x in (x1, x2)]
     assert lp1 == pytest.approx(lp2)
 
+def test_broadcast():
+    # Matrices
+    size_pairs = [((5,2), (5,3)), ((1,2), (5,3)), ((5,2), (1,3)), ((2,), (5,3)), ((5,2), (3,))]
+    out_sizes = [((5,2), (5,3))] * len(size_pairs)
+
+    for in_s, out_s in zip(size_pairs, out_sizes):
+        a,b = Flow._broadcast(jnp.ones(in_s[0]), jnp.ones(in_s[1]))
+        assert (a.shape, b.shape) == out_s
+
+
+def test_NeuralSplineFlow():
+    # Unconditional
+    n = 10
+    dim = 3
+    key = random.PRNGKey(2)
+    flow = NeuralSplineFlow(key, dim, num_layers=2)
+    x = flow.sample(key, n=n)
+    assert x.shape == (n, dim)
+
+    lp = flow.log_prob(x)
+    assert lp.shape == (n,)
+
+    # Conditional
+    cond_dim = 2
+    flow = NeuralSplineFlow(key, dim, condition_dim=cond_dim, num_layers=2)
+    cond = random.uniform(key, (n, cond_dim))
+    x = flow.sample(key, condition=cond)
+    lp = flow.log_prob(x, cond)
+    assert lp.shape == (n,)
+
+    lp = flow.log_prob(x, jnp.ones(cond_dim))
+    assert lp.shape == (n,)
+
+    lp = flow.log_prob(jnp.ones(dim), cond)
+    assert lp.shape == (n,)
+
+    x = flow.sample(key, condition=jnp.ones(2), n=n)
+    assert x.shape == (n, dim)
 
 def test_RealNVPFlow():
     key = random.PRNGKey(1)
     flow = RealNVPFlow(key, 3)
-    x = flow.sample(key, n=10)
-    assert x.shape == (10, 3)
-
-    lp = flow.log_prob(x)
-    assert lp.shape == (10,)
-
-
-def test_NeuralSplineFlow():
-    key = random.PRNGKey(2)
-    flow = NeuralSplineFlow(key, 3, num_layers=2)
     x = flow.sample(key, n=10)
     assert x.shape == (10, 3)
 