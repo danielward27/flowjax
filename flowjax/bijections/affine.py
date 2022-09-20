220d2f790ed42965f7c65e03b36f1c911a5fe230
danielward27 <danielward27@outlook.com>
2022-09-20 18:19:00 +0100

    Add affine and triangular affine transformation

--- flowjax/bijections/affine.py
+++ flowjax/bijections/affine.py
@@ -1,6 +1,8 @@
 from flowjax.bijections.abc import Bijection
 from flowjax.utils import Array
 import jax.numpy as jnp
+from flowjax.bijections.masked_autoregressive import rank_based_mask
+from jax.scipy.linalg import solve_triangular
 
 
 class Affine(Bijection):
@@ -28,5 +30,72 @@ class Affine(Bijection):
     @property
     def scale(self):
         return jnp.exp(self.log_scale)
+
+
+class TriangularAffine(Bijection):
+    loc: Array
+    dim: int
+    cond_dim: int
+    diag_mask: Array
+    tri_mask: Array
+    lower: bool
+    min_diag: float
+    _arr: Array
+    _log_diag: Array
+
+    def __init__(self, loc: Array, arr: Array, lower: bool = True, min_diag: float = 1e-6):
+        """
+        Transformation of the form Ax + b, where A is a lower or upper triangular matrix. To
+        ensure invertiblility, diagonal entries should be positive (and greater than min_diag).
+
+        Args:
+            loc (Array): Translation.
+            arr (Array): Matrix.
+            lower (bool, optional): Whether the mask should select the lower or upper triangular matrix (other elements ignored). Defaults to True.
+            min_diag (float, optional): Minimum value on the diagonal, to ensure invertibility. Defaults to 1e-6.
+        """
+        
+        if (arr.ndim != 2) or (arr.shape[0] != arr.shape[1]):
+            ValueError("arr must be a square, 2-dimensional matrix.")
+        if jnp.any(jnp.diag(arr) < min_diag):
+            ValueError("arr diagonal entries must be greater than min_diag")
+        
+        self.loc = loc
+        self.dim = arr.shape[0]
+        self.cond_dim = 0
+        self._arr = arr
+        self.diag_mask = jnp.eye(self.dim)
+        tri_mask = rank_based_mask(jnp.arange(self.dim), jnp.arange(self.dim))
+        self.tri_mask = tri_mask if lower else tri_mask.T
+        self.min_diag = min_diag
+        self._log_diag = jnp.log(jnp.diag(arr) - min_diag)
+        self.lower = lower
+        
+    @property
+    def arr(self):
+        "Get triangular array, with "
+        diag = self.diag_mask*jnp.exp(self._log_diag) + self.min_diag
+        return self.tri_mask*self._arr + diag
+
+    def transform(self, x, condition = None):
+        return self.arr @ x + self.loc
+
+    def transform_and_log_abs_det_jacobian(self, x, condition = None):
+        a = self.arr
+        return a @ x + self.loc, jnp.diag(a).sum()
+
+    def inverse(self, y, condition = None):
+        return solve_triangular(self.arr, y - self.loc, lower=self.lower)
+
+    def inverse_and_log_abs_det_jacobian(self, y, condition = None):
+        a = self.arr
+        return solve_triangular(a, y - self.loc, lower=self.lower), -jnp.diag(a).sum()
+
+    
+    
+
+
+
+        
 
         
