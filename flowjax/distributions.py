84cf80f25dde929c0d5d621cbd7d1cb964964cc7
tennessee_wallaceh <tennessee.wallaceh@gmail.com>
2022-09-22 11:03:32 +0100

    Revert added distributions

--- flowjax/distributions.py
+++ flowjax/distributions.py
@@ -150,7 +150,6 @@ class Transformed(Distribution):
         return x
 
 
-
 class Normal(Distribution):
     "Standard normal distribution, condition is ignored."
     def __init__(self, dim):
@@ -167,81 +166,3 @@ class Normal(Distribution):
     def __repr__(self):
         return f'<FlowJax StandardNormal>'
 
-class Uniform(Distribution):
-    "Standard uniform distribution, condition is ignored."
-    def __init__(self, dim, minval=0.0, maxval=1.0):
-        self.dim = dim
-        self.cond_dim = 0
-        self.loc = minval
-        self.scale = maxval - minval
-
-    def _log_prob(self, x: Array, condition: Optional[Array] = None):
-        assert x.shape == (self.dim,)
-        return stats.uniform.logpdf(x, loc=self.loc, scale=self.scale).sum()
-
-    def _sample(self, key: KeyArray, condition: Optional[Array] = None):
-        return random.uniform(
-            key, shape=(self.dim,), minval=self.loc, maxval=self.scale + self.loc
-        )
-
-    def __repr__(self):
-        return f'<FlowJax Uniform({self.loc}, {self.scale + self.loc})>'
-
-class Gumbel(Distribution):
-    """
-    Implements standard gumbel distribution.
-    loc=0, scale=1
-    """
-    def __init__(self, dim):
-        self.dim = dim
-        self.cond_dim = 0
-
-    def _log_prob(self, x: Array, condition: Optional[Array] = None):
-        assert x.shape == (self.dim,)
-        return -(x + jnp.exp(-x))
-
-    def _sample(self, key: KeyArray, condition: Optional[Array] = None):
-        return random.gumbel(key, shape=(self.dim,))
-
-class Cauchy(Distribution):
-    """
-    Implements standard cauchy distribution.
-    loc=0, scale=1
-    """
-    def __init__(self, dim):
-        self.dim = dim
-        self.cond_dim = 0
-
-    def _log_prob(self, x: Array, condition: Optional[Array] = None):
-        assert x.shape == (self.dim,)
-        return stats.cauchy.logpdf(x)
-
-    def _sample(self, key: KeyArray, condition: Optional[Array] = None):
-        return random.cauchy(key, shape=(self.dim,))
-
-class Exponential(Distribution):
-    """
-    Implements standard exponential distribution.
-    loc=0, scale=1
-    """
-    def __init__(self, dim):
-        self.dim = dim
-        self.cond_dim = 0
-
-    def _log_prob(self, x: Array, condition: Optional[Array] = None):
-        assert x.shape == (self.dim,)
-        return stats.expon.logpdf(x)
-
-        Args:
-            dim (int): Dimension of the normal distribution.
-        """
-        self.dim = dim
-        self.cond_dim = 0
-        self.df = jnp.array([5.])
-
-    def _log_prob(self, x: Array, condition: Optional[Array] = None):
-        assert x.shape == (self.dim,)
-        return stats.norm.logpdf(x).sum()
-
-    def _sample(self, key: KeyArray, condition: Optional[Array] = None):
-        return random.t(key, self.df, shape=(self.dim,))
