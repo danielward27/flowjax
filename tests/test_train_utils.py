260dc8556a54c27bad5bb3d9a02a725f12b7c147
danielward27 <danielward27@outlook.com>
2022-09-23 14:29:23 +0100

    Add filter_spec argument to train_flow

--- tests/test_train_utils.py
+++ tests/test_train_utils.py
@@ -1,9 +1,12 @@
-from flowjax.train_utils import count_fruitless
 import pytest
-from flowjax.train_utils import train_val_split, random_permutation_multiple
+from flowjax.distributions import Normal, Transformed
+from flowjax.bijections.affine import Affine
+from flowjax.train_utils import count_fruitless
+from flowjax.train_utils import train_val_split, random_permutation_multiple, train_flow
 import jax.numpy as jnp
 from jax import random
-
+import jax.tree_util as jtu
+import equinox as eqx
 
 def test_count_fruitless():
     assert count_fruitless([12, 2, 3, 4]) == 2
@@ -29,3 +32,27 @@ def test_random_permutation_multiple():
     x2, y2 = random_permutation_multiple(random.PRNGKey(0), (x, y))
     after = jnp.sort(jnp.concatenate((x2, y2), axis=1).sum(axis=1))
     assert (before == after).all()
+
+def test_train_flow_filter_spec():
+    dim = 3
+    base_dist = Transformed(Normal(dim), Affine(jnp.ones(dim), jnp.ones(dim)))
+    flow = Transformed(base_dist, Affine(jnp.ones(dim), jnp.ones(dim)))
+
+    # All params should change by default
+    before = eqx.filter(flow, eqx.is_inexact_array)
+    x = random.normal(random.PRNGKey(0), (100, dim))
+    flow, _ = train_flow(random.PRNGKey(0), flow, x, max_epochs=1, batch_size=50)
+    after = eqx.filter(flow, eqx.is_inexact_array)
+
+    assert jnp.all(before.base_dist.bijection.loc != after.base_dist.bijection.loc)
+    assert jnp.all(before.bijection.loc != after.bijection.loc)
+
+    # But we can provide a filter spec to avoid e.g. training the base distribution parameters.
+    before = eqx.filter(flow, eqx.is_inexact_array)
+    filter_spec = jtu.tree_map(lambda x: eqx.is_inexact_array(x), flow)
+    filter_spec = eqx.tree_at(lambda tree: tree.base_dist, filter_spec, replace=False)
+    flow, _ = train_flow(random.PRNGKey(0), flow, x, max_epochs=1, batch_size=50, filter_spec=filter_spec)
+    after = eqx.filter(flow, eqx.is_inexact_array)
+
+    assert jnp.all(before.base_dist.bijection.loc == after.base_dist.bijection.loc)
+    assert jnp.all(before.bijection.loc != after.bijection.loc)
