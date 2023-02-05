16cc6c0e19cb401a10763c802eb87150dc1dfac5
danielward27 <danielward27@outlook.com>
2023-02-05 13:05:57 +0000

    Add __all__ to train init

--- flowjax/train/__init__.py
+++ flowjax/train/__init__.py
@@ -1,2 +1,7 @@
 from flowjax.train.data_fit import fit_to_data
 from flowjax.train.variational_fit import fit_to_variational_target
+
+__all__ = [
+    "fit_to_data",
+    "fit_to_variational_target",
+]
