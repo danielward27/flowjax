b3449708b58bfc9211d7127b7975467690d1171e
danielward27 <danielward27@outlook.com>
2023-05-04 17:08:40 +0100

    rename fn

--- flowjax/bijections/jax_transforms.py
+++ flowjax/bijections/jax_transforms.py
@@ -78,35 +78,37 @@ class Batch(Bijection):
     def transform(self, x, condition=None):
         self._argcheck(x, condition)
 
-        def fn(bijection, x, condition):
+        def _transform(bijection, x, condition):
             return bijection.transform(x, condition)
 
-        return self.multi_vmap(fn)(self.bijection, x, condition)
+        return self.multi_vmap(_transform)(self.bijection, x, condition)
 
     def transform_and_log_det(self, x, condition=None):
         self._argcheck(x, condition)
 
-        def fn(bijection, x, condition):
+        def _transform_and_log_det(bijection, x, condition):
             return bijection.transform_and_log_det(x, condition)
 
-        y, log_det = self.multi_vmap(fn)(self.bijection, x, condition)
+        y, log_det = self.multi_vmap(_transform_and_log_det)(
+            self.bijection, x, condition
+        )
         return y, jnp.sum(log_det)
 
     def inverse(self, y, condition=None):
         self._argcheck(y, condition)
 
-        def fn(bijection, x, condition):
+        def _inverse(bijection, x, condition):
             return bijection.inverse(x, condition)
 
-        return self.multi_vmap(fn)(self.bijection, y, condition)
+        return self.multi_vmap(_inverse)(self.bijection, y, condition)
 
     def inverse_and_log_det(self, y, condition=None):
         self._argcheck(y, condition)
 
-        def fn(bijection, x, condition):
+        def _inverse_and_log_det(bijection, x, condition):
             return bijection.inverse_and_log_det(x, condition)
 
-        x, log_det = self.multi_vmap(fn)(self.bijection, y, condition)
+        x, log_det = self.multi_vmap(_inverse_and_log_det)(self.bijection, y, condition)
         return x, jnp.sum(log_det)
 
     def multi_vmap(self, func: Callable) -> Callable:
@@ -153,49 +155,49 @@ class Scan(Bijection):
     def transform(self, x, condition=None):
         self._argcheck(x, condition)
 
-        def fn(x, p, condition=None):
-            bijection = eqx.combine(self.static, p)
+        def step(x, params, condition=None):
+            bijection = eqx.combine(self.static, params)
             result = bijection.transform(x, condition)  # type: ignore
             return (result, None)
 
-        fn = partial(fn, condition=condition)
-        y, _ = scan(fn, x, self.params)
+        step = partial(step, condition=condition)
+        y, _ = scan(step, x, self.params)
         return y
 
     def transform_and_log_det(self, x, condition=None):
         self._argcheck(x, condition)
 
-        def fn(carry, params, condition):
+        def step(carry, params, condition):
             x, log_det = carry
             bijection = eqx.combine(self.static, params)
             y, log_det_i = bijection.transform_and_log_det(x, condition)  # type: ignore
             return ((y, log_det + log_det_i.sum()), None)
 
-        fn = partial(fn, condition=condition)
-        (y, log_det), _ = scan(fn, (x, 0), self.params)
+        step = partial(step, condition=condition)
+        (y, log_det), _ = scan(step, (x, 0), self.params)
         return y, log_det
 
     def inverse(self, y, condition=None):
         self._argcheck(y, condition)
 
-        def fn(y, params, condition):
+        def step(y, params, condition):
             bijection = eqx.combine(self.static, params)
             x = bijection.inverse(y, condition)  # type: ignore
             return (x, None)
 
-        fn = partial(fn, condition=condition)
-        x, _ = scan(fn, y, self.params, reverse=True)
+        step = partial(step, condition=condition)
+        x, _ = scan(step, y, self.params, reverse=True)
         return x
 
     def inverse_and_log_det(self, y, condition=None):
         self._argcheck(y, condition)
 
-        def fn(carry, params, condition):
+        def step(carry, params, condition):
             y, log_det = carry
             bijection = eqx.combine(self.static, params)
             x, log_det_i = bijection.inverse_and_log_det(y, condition)  # type: ignore
             return ((x, log_det + log_det_i.sum()), None)
 
-        fn = partial(fn, condition=condition)
-        (y, log_det), _ = scan(fn, (y, 0), self.params, reverse=True)
+        step = partial(step, condition=condition)
+        (y, log_det), _ = scan(step, (y, 0), self.params, reverse=True)
         return y, log_det
