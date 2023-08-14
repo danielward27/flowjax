b9f249cb9f590cfe7a113b2be2ab7a73988aa051
danielward27 <danielward27@outlook.com>
2023-08-14 13:46:21 +0100

    Generalise VI code

--- flowjax/train/variational_fit.py
+++ flowjax/train/variational_fit.py
@@ -2,9 +2,9 @@
 from typing import Any, Callable
 
 import equinox as eqx
+import jax.random as jr
 import optax
 from jax import Array
-import jax.random as jr
 from jax.typing import ArrayLike
 from tqdm import tqdm
 
@@ -13,47 +13,26 @@ from flowjax.distributions import Distribution
 PyTree = Any
 
 
-def elbo_loss(
-    dist: Distribution,
-    target: Callable[[ArrayLike, ArrayLike | None], ArrayLike],
-    key: jr.KeyArray,
-    num_samples: int,
-) -> Array:
-    samples, approx_density = dist.sample_and_log_prob(key, (num_samples,))
-    target_density = target(samples)
-    losses = approx_density - target_density
-    return losses.mean()
-
-
-class Elbo:
+class ElboLoss:
     """Elbo loss function, approximated using samples."""
 
-    num_particles: int
-    target: Callable[[ArrayLike, ArrayLike | None], Array]
+    target: Callable[[ArrayLike], Array]
+    num_samples: int
 
-    def __init__(
-        self, num_particles: int, target: Callable[[ArrayLike, ArrayLike | None], Array]
-    ):
+    def __init__(self, target: Callable[[ArrayLike], Array], num_samples: int):
         """
         Args:
-            num_particles (int): Number of samples to use in the ELBO approximation.
-            target (Callable[[ArrayLike, ArrayLike | None], Array]): The target,
-                e.g. log posterior density up to an additive constant. This must take
-                two arguments, an array of points to evaluate, and corresponding
-                conditioning variables (or None, if an unconditional distribution is
-                used).
+            num_samples (int): Number of samples to use in the ELBO approximation.
+            target (Callable[[ArrayLike], Array]): The target,
+                e.g. log posterior density up to an additive constant.
         """
-        self.num_particles = num_particles
         self.target = target
+        self.num_samples = num_samples
 
-    def __call__(
-        self, dist: Distribution, key: jr.KeyArray, condition: ArrayLike | None = None
-    ):
+    def __call__(self, dist: Distribution, key: jr.KeyArray):
         """Computes an estimate of the negative ELBO loss."""
-        samples, log_probs = dist.sample_and_log_prob(
-            key, (self.num_particles,), condition
-        )
-        target_density = self.target(samples, condition)
+        samples, log_probs = dist.sample_and_log_prob(key, (self.num_samples,))
+        target_density = self.target(samples)
         losses = log_probs - target_density
         return losses.mean()
 
@@ -61,7 +40,7 @@ class Elbo:
 def fit_to_variational_target(
     key: jr.KeyArray,
     dist: Distribution,
-    loss_fn: Callable[[Distribution, jr.KeyArray, ArrayLike | None], Array],
+    loss_fn: Callable[[Distribution, jr.KeyArray], Array],
     steps: int = 100,
     learning_rate: float = 5e-4,
     clip_norm: float = 0.5,
@@ -69,34 +48,31 @@ def fit_to_variational_target(
     filter_spec: Callable | PyTree = eqx.is_inexact_array,
     show_progress: bool = True,
 ):
-    """
-    Train a distribution (e.g. a flow) by variational inference to a target
+    """Train a distribution (e.g. a flow) by variational inference to a target
     (e.g. an unnormalized density).
 
     Args:
         key (jr.KeyArray): Jax PRNGKey.
         dist (Distribution): Distribution object, trainable parameters are found
             using equinox.is_inexact_array.
-        target (Callable[[Array], Array]): The variational target (this is usually the unormalized log
-            posterior)
-        loss_fn (Callable[[Distribution, Callable, jr.KeyArray, int], Array]): The loss
-            function to optimize. The loss function should take the distribution to train,
-            the target function, key and an int (number of samples) and map to a scalar
-            loss. Defaults to elbo_loss.
-        steps (int): The number of training steps to run. Defaults to 100.
-        samples_per_step (int): number of samples to use at each step.
-            Defaults to 500.
-        learning_rate (float): Adam learning rate. Defaults to 5e-4.
-        clip_norm (float): Maximum gradient norm before clipping occurs.
+        loss_fn (Callable[[Distribution, jr.KeyArray], Array]): The loss function to
+            optimize (e.g. the ElboLoss). The loss function should take the distribution
+            to train and a key.
+        steps (int, optional): The number of training steps to run. Defaults to 100.
+        learning_rate (float, optional): Learning rate. Defaults to 5e-4.
+        clip_norm (float, optional): Maximum gradient norm before clipping occurs.
             Defaults to 0.5.
-        optimizer (optax.GradientTransformation | None): Optax optimizer. If provided,
-            this overrides the default Adam optimizer, and the learning_rate and
-            clip_norm arguments are ignored. Defaults to None.
-        filter_spec (Callable | PyTree): Equinox `filter_spec` for
+        optimizer (optax.GradientTransformation | None, optional): Optax optimizer. If
+            provided, this overrides the default Adam optimizer, and the learning_rate
+            and clip_norm arguments are ignored. Defaults to None.
+        filter_spec (Callable | PyTree, optional): Equinox `filter_spec` for
             specifying trainable parameters. Either a callable `leaf -> bool`, or a
             PyTree with prefix structure matching `dist` with True/False values.
-            Defaults to `eqx.is_inexact_array`.
-        show_progress (bool): Whether to show progress bar. Defaults to True.
+            Defaults to eqx.is_inexact_array.
+        show_progress (bool, optional): Whether to show progress bar. Defaults to True.
+
+    Returns:
+        tuple: (distribution, losses).
     """
     if optimizer is None:
         optimizer = optimizer = optax.chain(
@@ -104,34 +80,31 @@ def fit_to_variational_target(
             optax.adam(learning_rate=learning_rate),
         )
 
+    @eqx.filter_value_and_grad
+    def loss_val_and_grad(trainable, static, key):
+        model = eqx.combine(trainable, static)
+        return loss_fn(model, key)
+
     @eqx.filter_jit
-    def step(dist, target, key, optimizer, opt_state):
-        @eqx.filter_value_and_grad
-        def loss_val_and_grad(
-            dist_trainable, dist_static, target, key, samples_per_step
-        ):
-            dist = eqx.combine(dist_trainable, dist_static)
-            return loss_fn(dist, target, key, samples_per_step)
-
-        dist_trainable, dist_static = eqx.partition(dist, filter_spec)
-        loss_val, grads = loss_val_and_grad(
-            dist_trainable, dist_static, target, key, samples_per_step
-        )
+    def step(trainable, static, key, optimizer, opt_state):
+        loss_val, grads = loss_val_and_grad(trainable, static, key)
         updates, opt_state = optimizer.update(grads, opt_state)
-        dist = eqx.apply_updates(dist, updates)
-        return dist, opt_state, loss_val
+        trainable = eqx.apply_updates(trainable, updates)
+        return trainable, opt_state, loss_val
 
-    dist_trainable, _ = eqx.partition(dist, filter_spec)  # type: ignore
-    opt_state = optimizer.init(dist_trainable)
+    params, static = eqx.partition(dist, filter_spec)
+    opt_state = optimizer.init(params)
 
     losses = []
-
     loop = tqdm(range(steps), disable=not show_progress)
+
+    best_params = params
     for _ in loop:
         key, subkey = jr.split(key)
-        dist, opt_state, loss = step(dist, target, subkey, optimizer, opt_state)  # type: ignore
-
+        params, opt_state, loss = step(params, static, subkey, optimizer, opt_state)  # type: ignore
         losses.append(loss.item())
-        loop.set_postfix({"loss": losses[-1]})
+        loop.set_postfix({"loss": loss.item()})
+        if loss.item() == min(losses):
+            best_params = params
 
-    return dist, losses
+    return eqx.combine(best_params, static), losses
--- tests/train/test_variational_fit.py
+++ tests/train/test_variational_fit.py
@@ -1,92 +1,39 @@
-import equinox as eqx
-import jax
 import jax.numpy as jnp
+import jax.random as jr
 import pytest
-from jax import random
 
-from flowjax import bijections, distributions
-from flowjax.flows import MaskedAutoregressiveFlow
-from flowjax.train.variational_fit import elbo_loss, fit_to_variational_target
+from flowjax.distributions import Normal, StandardNormal
+from flowjax.train.variational_fit import ElboLoss, fit_to_variational_target
 
-
-@pytest.mark.parametrize(
-    "distribution, target, shape",
-    [
-        pytest.param(
-            distributions.StandardNormal,
-            distributions.StandardNormal,
-            (2,),
-            id="normal_target_normal",
-        ),
-        pytest.param(
-            distributions.StandardNormal,
-            distributions._StandardCauchy,
-            (2,),
-            id="normal_target_cauchy",
-        ),
-        pytest.param(
-            distributions.StandardNormal,
-            distributions.StandardNormal,
-            (100,),
-            id="higher_dim",
-        ),
-        pytest.param(
-            distributions.StandardNormal,
-            distributions.StandardNormal,
-            (3, 2, 2),
-            id="multiple_dim",
-        ),
-    ],
-)
-def test_elbo_loss(distribution, target, shape):
-    distribution_object: distributions.Distribution = distribution(shape)
-    target = target(shape).log_prob
-
-    loss = elbo_loss(distribution_object, target, random.PRNGKey(0), num_samples=100)
-
-    assert loss.shape == ()  # expect scalar loss
-    assert jnp.isfinite(loss)  # expect finite loss
+test_shapes = [(), (2,), (2, 3, 4)]
 
 
-def test_fit_to_variational_target_e2e():
-    # A simple E2E test to make sure that the function runs without error
-    flow_random_key = random.PRNGKey(10)
-    flow = MaskedAutoregressiveFlow(
-        key=flow_random_key,
-        base_dist=distributions.StandardNormal((5,)),
-        flow_layers=4,
-        transformer=bijections.RationalQuadraticSpline(knots=3, interval=6),
-    )
+@pytest.mark.parametrize("shape", test_shapes)
+def test_elbo_loss(shape):
+    "Check finite scaler loss."
+    target = StandardNormal(shape)
+    vi_dist = StandardNormal(shape)
+    loss_fn = ElboLoss(target.log_prob, num_samples=100)
+    loss_val = loss_fn(vi_dist, jr.PRNGKey(0))
+    assert loss_val.shape == ()  # expect scalar loss
+    assert jnp.isfinite(loss_val)  # expect finite loss
 
-    # Target a shifted + scaled normal
-    target_dist = distributions.Normal(
-        1.5 * jnp.ones(5),
-        0.5 * jnp.ones(5),
-    )
-    target = target_dist.log_prob
 
-    train_random_key = random.PRNGKey(0)
-    trained_flow, losses = fit_to_variational_target(
-        key=train_random_key,
-        dist=flow,
-        target=target,
-        steps=10,
+@pytest.mark.parametrize("shape", test_shapes)
+def test_fit_to_variational_target(shape):
+    "Check that loss decreases."
+    vi_dist = Normal(jnp.ones(shape))
+    target_dist = StandardNormal(shape)
+
+    loss = ElboLoss(target_dist.log_prob, 50)
+
+    vi_dist, losses = fit_to_variational_target(
+        key=jr.PRNGKey(0),
+        dist=vi_dist,
+        loss_fn=loss,
         show_progress=False,
+        learning_rate=0.1,
     )
-
-    # Check that we have trained the flow
-    initial_params, initial_static = eqx.partition(flow, eqx.is_inexact_array)  # type: ignore
-    trained_params, trained_static = eqx.partition(trained_flow, eqx.is_inexact_array)  # type: ignore
-
-    assert initial_static == trained_static
-    jax.tree_util.tree_map(
-        lambda trained_param, initial_param: not jnp.allclose(
-            trained_param, initial_param
-        ),
-        trained_params,
-        initial_params,
-    )
-
     # We expect the loss to be decreasing
     start, end = jnp.split(jnp.array(losses), 2)
     assert jnp.mean(start) > jnp.mean(end)
