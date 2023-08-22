3b9b26a87c2c627fedfc14bba025179ad3fee23e
danielward27 <danielward27@outlook.com>
2023-08-22 09:51:49 +0100

    Allow axis specification in train_val_split

--- flowjax/train/train_utils.py
+++ flowjax/train/train_utils.py
@@ -6,26 +6,31 @@ import jax.random as jr
 from jax import Array
 
 
-def train_val_split(key: jr.KeyArray, arrays: Sequence[Array], val_prop: float = 0.1):
-    """Train validation split along axis 0.
+def train_val_split(
+    key: jr.KeyArray, arrays: Sequence[Array], val_prop: float = 0.1, axis: int = 0
+):
+    """Random train validation split along a given axis.
 
     Args:
         key (KeyArray): Jax PRNGKey
-        arrays Sequence[Array]: Sequence of arrays.
+        arrays Sequence[Array]: Sequence of arrays, with matching size on specified axis.
         val_prop (float): Proportion of data to use for validation. Defaults to 0.1.
+        axis (int): Axis along which to split. Defaults to 0.
 
     Returns:
         tuple[tuple]: (train_arrays, validation_arrays)
     """
     if not 0 <= val_prop <= 1:
         raise ValueError("val_prop should be between 0 and 1.")
-    size_axis_0 = arrays[0].shape[0]
-    permutation = jr.permutation(key, jnp.arange(size_axis_0))
-    arrays = tuple(a[permutation] for a in arrays)
-    n_train = size_axis_0 - round(val_prop * size_axis_0)
-    train = tuple(a[:n_train] for a in arrays)
-    val = tuple(a[n_train:] for a in arrays)
-    return train, val
+    num_samples = arrays[0].shape[axis]
+    if not all(a[0].shape[axis] == num_samples for a in arrays):
+        raise ValueError(f"Array dimensions do not match along axis {axis}.")
+    permutation = jr.permutation(key, num_samples)
+    n_train = num_samples - round(val_prop * num_samples)
+    train_indices, val_indices = permutation[:n_train], permutation[n_train:]
+    train_arrays = tuple(jnp.take(array, train_indices, axis=axis) for array in arrays)
+    val_arrays = tuple(jnp.take(array, val_indices, axis=axis) for array in arrays)
+    return train_arrays, val_arrays
 
 
 def count_fruitless(losses: list[float]) -> int:
