Fix autoregressive masking

flowjax/bijections/masked_autoregressive.py
@@ -63,12 +63,13 @@ class MaskedAutoregressive(AbstractBijection):
         if cond_dim is None:
             self.cond_shape = None
             in_ranks = jnp.arange(dim)
+            hidden_ranks = jnp.arange(nn_width) % (dim - 1)
+            # If dim=1, hidden ranks all zero -> all weights masked out in final layer
         else:
             self.cond_shape = (cond_dim,)
             # we give conditioning variables rank -1 (no masking of edges to output)
             in_ranks = jnp.hstack((jnp.arange(dim), -jnp.ones(cond_dim, int)))
-
-        hidden_ranks = jnp.arange(nn_width) % dim
+            hidden_ranks = (jnp.arange(nn_width) % dim) - 1
         out_ranks = jnp.repeat(jnp.arange(dim), num_params)
 
         self.masked_autoregressive_mlp = masked_autoregressive_mlp(