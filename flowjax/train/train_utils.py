7273056528cae756a6a516513ed21737ce0e7fee
danielward27 <danielward27@outlook.com>
2025-02-28 11:43:51 +0000

    Generalize train_val_split and get_batches to pytrees

--- flowjax/train/train_utils.py
+++ flowjax/train/train_utils.py
@@ -1,14 +1,15 @@
 """Utility functions for training."""
 
-from collections.abc import Callable, Sequence
+from collections.abc import Callable
 from functools import partial
 
 import equinox as eqx
+import jax
 import jax.numpy as jnp
 import jax.random as jr
 import optax
 from jax import jit
-from jaxtyping import Array, PRNGKeyArray, PyTree, Scalar, Shaped
+from jaxtyping import Array, PRNGKeyArray, PyTree, Scalar
 
 
 @eqx.filter_jit
@@ -47,14 +48,14 @@ def step(
 
 def train_val_split(
     key: PRNGKeyArray,
-    arrays: Sequence[Shaped[Array, "batch ..."]],
+    arrays: PyTree[Array],
     val_prop: float = 0.1,
-):
+) -> tuple[PyTree[Array], PyTree[Array]]:
     """Random train validation split for a sequence of arrays.
 
     Args:
         key: Jax random key.
-        arrays: Sequence of arrays, with matching size on axis 0.
+        arrays: A pytree of arrays (e.g. a tuple), with matching size on axis 0.
         val_prop: Proportion of data to use for validation. Defaults to 0.1.
 
     Returns:
@@ -63,19 +64,20 @@ def train_val_split(
     if not 0 <= val_prop <= 1:
         raise ValueError("val_prop should be between 0 and 1.")
 
-    num_samples = arrays[0].shape[0]
-    if not all(isinstance(a, Shaped[Array, " dim ..."]) for a in arrays):
+    leaves = jax.tree.leaves(arrays)
+    num_samples = leaves[0].shape[0]
+    if not all(leaf.shape[0] == num_samples for leaf in leaves):
         raise ValueError("Array dimensions must match along axis 0.")
 
     n_train = num_samples - round(val_prop * num_samples)
-    arrays = [jr.permutation(key, a) for a in arrays]
-    train_arrays = [arr[:n_train] for arr in arrays]
-    val_arrays = [arr[n_train:] for arr in arrays]
+    arrays = jax.tree.map(lambda arr: jr.permutation(key, arr), arrays)
+    train_arrays = jax.tree.map(lambda arr: arr[:n_train], arrays)
+    val_arrays = jax.tree.map(lambda arr: arr[n_train:], arrays)
     return train_arrays, val_arrays
 
 
 @partial(jit, static_argnums=1)
-def get_batches(arrays: Sequence[Array], batch_size: int):
+def get_batches(arrays: PyTree[Array], batch_size: int) -> PyTree[Array]:
     """Reshape a sequence of arrays to have an additional dimension for the batches.
 
     Specifically, this will transform an array with shape ``(data_len, *rest)`` to
@@ -88,14 +90,13 @@ def get_batches(arrays: Sequence[Array], batch_size: int):
             batch size to be equal to the data length.
 
     Args:
-        arrays: Sequence of arrays, with shape matching on axis 0.
+        arrays: Pytree of arrays (e.g. a tuple) with shape matching on axis 0.
         batch_size: The batch size.
     """
-    data_len = arrays[0].shape[0]
-    if not all(arr.shape[0] == data_len for arr in arrays):
-        raise ValueError("Array dimensions do not match along the batch axis.")
-
-    return tuple(_add_batch(arr, batch_size) for arr in arrays)
+    leaves = jax.tree.leaves(arrays)
+    if not all(leaves[0].shape[0] == leaf.shape[0] for leaf in leaves):
+        raise ValueError("Array dimensions must match along axis 0.")
+    return jax.tree.map(lambda arr: _add_batch(arr, batch_size), arrays)
 
 
 def _add_batch(arr, batch_size):
--- tests/test_train/test_train_utils.py
+++ tests/test_train/test_train_utils.py
@@ -1,4 +1,5 @@
 "Tests for train_utils.py."
+import jax
 import jax.numpy as jnp
 import jax.random as jr
 import pytest
@@ -8,24 +9,32 @@ from flowjax.train.train_utils import count_fruitless, get_batches, train_val_sp
 
 def test_train_val_split():
     key = jr.key(0)
-    arrays = [jnp.ones(5), jnp.ones((5, 2)), jnp.ones((5, 3, 3))]
+    arrays = [jnp.ones(5), jnp.ones((5, 2)), None, jnp.ones((5, 3, 3))]
 
     train, val = train_val_split(key, arrays, val_prop=0.2)
 
-    expected_train = [(4,), (4, 2), (4, 3, 3)]
-    expected_val = [(1,) + s[1:] for s in expected_train]
+    train_shapes = jax.tree.map(jnp.shape, train)
+    val_shapes = jax.tree.map(jnp.shape, val)
 
-    # Check shapes
+    expected_train_shapes = [(4,), (4, 2), None, (4, 3, 3)]
+    expected_val_shapes = [(1,), (1, 2), None, (1, 3, 3)]
+
     assert all(
-        train_arr.shape == expected
-        for train_arr, expected in zip(train, expected_train, strict=True)
+        train_shape == expected_train_shape
+        for train_shape, expected_train_shape in zip(
+            train_shapes,
+            expected_train_shapes,
+            strict=True,
+        )
     )
     assert all(
-        val_arr.shape == expected
-        for val_arr, expected in zip(val, expected_val, strict=True)
+        val_shape == expected_val_shape
+        for val_shape, expected_val_shape in zip(
+            val_shapes, expected_val_shapes, strict=True
+        )
     )
 
-    arrays = [jnp.ones((5, 2)), jnp.ones((3, 5))]
+    arrays = [jnp.ones((5, 2)), jnp.ones((3, 5)), None]
 
     # Axis mismatch
     with pytest.raises(ValueError, match="Array dimensions must match along axis 0"):
@@ -39,6 +48,8 @@ def test_count_fruitless():
 
 
 def test_get_batches():
-    arrays = [jnp.arange(26).reshape(13, 2)] * 2
+    arrays = [jnp.arange(26).reshape(13, 2), jnp.arange(26).reshape(13, 2), None]
     out = get_batches(arrays, batch_size=4)
     assert out[0].shape == (3, 4, 2)
+    assert out[1].shape == (3, 4, 2)
+    assert out[2] is None
